85e691d7b19b333ae9dd3b5b51cc60b60a46600c
Minserok Choi <min.choi.dev@gmail.com>
2018-11-12 20:49:44 -0500

    EditRecruits recoved

--- DGFScouting/ConnectionClass.cs
+++ DGFScouting/ConnectionClass.cs
@@ -397,6 +397,76 @@ namespace DGFScouting
             }
         }
 
+
+        // 11/11/18_Yayun Yang (Kim)
+        // EditRecruit() takes 14 arguments, connects to the database, attempts to update existed record and returns bool
+        public static bool EditRecruit(int id, string firstName, string lastName, string contactNumber, string emailAddress, int birthyear, int graduationYear, string currentTeam, int jerseyNumber, string position, string mothersName, string fathersName, string recruitStatus, string dateAdded)
+        {
+            string query = string.Format(@"Update Recruit SET FirstName='{1}', LastName='{2}', ContactNumber='{3}', EmailAddress='{4}', BirthYear='{5}', GraduationYear='{6}', CurrentTeam='{7}', 
+                                            JerseyNumber='{8}', Position='{9}', MothersName='{10}', FathersName='{11}', Status='{12}', DateAdded='{13}' WHERE RecruitID = {0}",
+                                        id, firstName, lastName, contactNumber, emailAddress, birthyear, graduationYear, currentTeam, jerseyNumber, position, mothersName, fathersName, recruitStatus, dateAdded);
+
+            cmd = new SqlCommand(query, cn);
+
+            try
+            {
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+
+            catch
+            {
+                return false;
+            }
+
+            finally
+            {
+                cn.Close();
+            }
+        }
+
+        // 11/11/18_Yayun Yang (Kim)
+        //DisplayRecruit() takes 1 arguement, connects to the database, retrieves 1 row of Recruit table of selected RecruitID, and returns RecruitClass object.
+        public static RecruitClass DisplayRecruit(int id)
+        {
+            RecruitClass recruit = new RecruitClass();
+            string query = string.Format(@"SELECT * FROM Recruit WHERE RecruitID=" + id);
+
+            cmd = new SqlCommand(query, cn);
+            try
+            {
+                cn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    recruit.FirstName = reader[1].ToString();
+                    recruit.LastName = reader[2].ToString();
+                    recruit.ContactNumber = reader[3].ToString();
+                    recruit.EmailAddress = reader[4].ToString();
+                    recruit.Birthyear = Convert.ToInt32(reader[5]);
+                    recruit.GraduationYear = Convert.ToInt32(reader[6]);
+                    recruit.CurrentTeam = reader[7].ToString();
+                    recruit.JerseyNumber = Convert.ToInt32(reader[8]);
+                    recruit.Position = reader[9].ToString();
+                    recruit.MothersName = reader[10].ToString();
+                    recruit.FathersName = reader[11].ToString();
+                    recruit.Status = reader[12].ToString();
+                    recruit.DateAdded = Convert.ToDateTime(reader[13]);
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return recruit;
+        }
+
     }
 
 }
