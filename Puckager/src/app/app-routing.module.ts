19555a3a8f742a43865cc7ac19f06f3e339dce46
Ryan <ryan.bg.watson@gmail.com>
2019-01-31 13:48:06 -0500

    Added registration component; Added Angular Document

--- Puckager/src/app/app-routing.module.ts
+++ Puckager/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'registration', component: RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
