3b0fa173acc054c7652fc3c4cab511f76363783b
Ryan <ryan.bg.watson@gmail.com>
2019-04-02 15:07:25 -0400

    Cors issue

--- client/src/app/services/recruit.service.ts
+++ client/src/app/services/recruit.service.ts
@@ -18,8 +18,8 @@ export class RecruitService {
   // *** REMINDER - must change endpoint uri to host url for it to work
   // private uri = 'http://localhost:3000/api/';
   private uri = 'https://puckager.herokuapp.com/api/';
-  private djangoUrl = '/twitter';
-  // private djangoUrl = 'https://puckager.herokuapp.com/twitter';
+  // private djangoUrl = '/twitter';
+  private djangoUrl = 'https://puckager-crawler.herokuapp.com/twitter/';
 
   private httpOptions = {
     headers: new HttpHeaders({
