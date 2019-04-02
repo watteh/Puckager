52e89fcabb437258c6daf2e7fb11643dbcf45d0b
Ryan <ryan.bg.watson@gmail.com>
2019-04-01 22:23:57 -0400

    changed origin

--- client/src/app/services/recruit.service.ts
+++ client/src/app/services/recruit.service.ts
@@ -87,7 +87,7 @@ export class RecruitService {
     const djangoHttpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://puckager-crawler.herokuapp.com/twitter',
+        'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
         'Access-Control-Allow-Methods': 'POST'
       })
