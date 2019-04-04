0317685c9a7a12213cea6f4250525aa31f2a9ee6
WickedDeveloper <gwyn@dgfprofessionalservices.com>
2019-04-04 00:58:59 -0400

    Update for production build.

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 // DB.js
 
 module.exports = {
-    "URI" : "mongodb://localhost/puckager",
-    //"URI": "mongodb://admin:()puckluck19@ds147995.mlab.com:47995/puckager",
+    //"URI" : "mongodb://localhost/puckager",
+    "URI": "mongodb://admin:()puckluck19@ds147995.mlab.com:47995/puckager",
     "secret": "someSecret"
 };
