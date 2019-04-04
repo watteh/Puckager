262839ea3ea18d838770278ab8d4a9392649e3d8
WickedDeveloper <gwyn@dgfprofessionalservices.com>
2019-04-04 02:58:45 -0400

    Build Production

--- client/src/app/services/recruit.service.ts
+++ client/src/app/services/recruit.service.ts
@@ -14,8 +14,8 @@ export class RecruitService {
   private authToken: any = null;
 
   // *** REMINDER - must change endpoint uri to host url for it to work
-  private uri = 'http://localhost:3000/api/';
-  //private uri = 'https://puckager.herokuapp.com/api/';
+  //private uri = 'http://localhost:3000/api/';
+  private uri = 'https://puckager.herokuapp.com/api/';
 
   private httpOptions = {
     headers: new HttpHeaders({
