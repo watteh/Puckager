e8d91becd2e19bee453b07b88bf26435bd959f28
DragoniCode <g7angel_hunter@hotmail.com>
2019-04-10 07:45:42 -0400

    stripped links from text pulls on twitter posts

--- client/src/app/recruit-list/recruit-social/recruit-social.component.ts
+++ client/src/app/recruit-list/recruit-social/recruit-social.component.ts
@@ -20,4 +20,15 @@ export class RecruitSocialComponent implements OnInit {
     });
   }
 
+  linkify(text) {
+    var urlRegex =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(urlRegex, function(url) {
+        return '<a href="' + url + '">' + url + '</a>';
+    });
+  }
+  removeURLs(text) {
+    text = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+    return text;
+  }
+  
 }
